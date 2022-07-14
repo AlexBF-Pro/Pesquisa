ea84af6b906f6ad2d31fd90b7d2d9a5ff4f9463e
AlexBF_Pro <alexfirmiano2@gmail.com>
2022-07-14 10:36:37 -0300

    Formulado os comando de votos

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 import './App.css';
 
-import { Resultado } from './components/Resultado';
 import { Votos } from './components/Votos';
 
 function App() {
   return (
     <div>
-      <Resultado /> <Votos />
+      <Votos />
     </div>
   );
 }
--- src/components/votos.tsx
+++ src/components/votos.tsx
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
 
 export default function Votos() {
-  const [votar, setVotar] = useState<number>();
+  const [numero, setNumero] = useState<string>();
+  const [listaCandidato, setListaCandidato] = useState([
+    { nome: 'Ciro Gomes', numero: '12', votos: 0 },
+    { nome: 'Jair Bolsonaro', numero: '22', votos: 0 },
+    { nome: 'Luiz Inácio Lula', numero: '13', votos: 0 },
+    { nome: 'Simone Tebet', numero: '15', votos: 0 }
+  ]);
 
   function voto() {
-    const informeValor = votar;
-    setVotar(informeValor);
+    const copy = [...listaCandidato];
+    const novaLista = copy.map((candidato) => {
+      if (candidato.numero === numero) {
+        return { ...candidato, votos: candidato.votos + 1 };
+      }
+      return candidato;
+    });
+    setListaCandidato(novaLista);
   }
 
   function quandoInputMudar(e: any) {
-    setVotar(e.target.value);
+    setNumero(e.target.value);
   }
 
   return (
     <div className="Apps">
+      <table>
+        <tr>
+          <th>Candidato</th>
+          <th>Número</th>
+          <th>Resultado Parcial</th>
+        </tr>
+        {listaCandidato.map((candidato) => (
+          <tr>
+            <td>{candidato.nome}</td>
+            <td>{candidato.numero}</td>
+            <td>{candidato.votos}</td>
+          </tr>
+        ))}
+      </table>
       <h4>Informe o número do Candidato de sua preferência abaixo.</h4>
       <form>
         <input
@@ -23,7 +49,7 @@ export default function Votos() {
           placeholder="Número do Candidato..."
         />
         <div className="butoes">
-          <button type="submit" onClick={voto}>
+          <button type="button" onClick={voto}>
             Votar
           </button>
         </div>
